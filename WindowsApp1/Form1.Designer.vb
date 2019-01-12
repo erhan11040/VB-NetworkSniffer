db123a0b4679f371011cf53ff973fb1804a0dddd
HeathCliff <erhan11040@gmail.com>
2019-01-12 22:07:05 +0300

    Project Uploaded

---
+++ WindowsApp1/Form1.Designer.vb
@@ -0,0 +1,114 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.DGV = New System.Windows.Forms.DataGridView()
+        Me.Column1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column3 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column4 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        CType(Me.DGV, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(12, 12)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(121, 21)
+        Me.ComboBox1.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(139, 10)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(243, 12)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 2
+        '
+        'DGV
+        '
+        Me.DGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DGV.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Column1, Me.Column2, Me.Column3, Me.Column4})
+        Me.DGV.Location = New System.Drawing.Point(12, 57)
+        Me.DGV.Name = "DGV"
+        Me.DGV.Size = New System.Drawing.Size(776, 381)
+        Me.DGV.TabIndex = 3
+        '
+        'Column1
+        '
+        Me.Column1.HeaderText = "Column1"
+        Me.Column1.Name = "Column1"
+        '
+        'Column2
+        '
+        Me.Column2.HeaderText = "Column2"
+        Me.Column2.Name = "Column2"
+        '
+        'Column3
+        '
+        Me.Column3.HeaderText = "Column3"
+        Me.Column3.Name = "Column3"
+        '
+        'Column4
+        '
+        Me.Column4.HeaderText = "Column4"
+        Me.Column4.Name = "Column4"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.DGV)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DGV, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents DGV As DataGridView
+    Friend WithEvents Column1 As DataGridViewTextBoxColumn
+    Friend WithEvents Column2 As DataGridViewTextBoxColumn
+    Friend WithEvents Column3 As DataGridViewTextBoxColumn
+    Friend WithEvents Column4 As DataGridViewTextBoxColumn
+End Class
---
+++ WindowsApp1/Form1.vb
@@ -0,0 +1,170 @@
+﻿Imports System.Net
+Imports System.Net.Sockets
+
+
+Public Class Form1
+    Dim socketz As New Socket(AddressFamily.InterNetwork, SocketType.Raw, ProtocolType.IP)
+    Dim bytedata(4096) As Byte
+    Dim myip As IPAddress
+    Dim started As Boolean = True
+    Dim sizediff As Size
+    Dim formloaded As Boolean = False
+    Dim FilterIPAddress As New IPAddress(0)
+    Dim FilterIP As Boolean
+    Dim mycomputerconnections() As Net.NetworkInformation.NetworkInterface
+
+    'DGV Update stuff
+    Dim stringz As String = ""
+    Dim Typez As String = ""
+    Dim ipfrom As IPAddress
+    Dim ipto As IPAddress
+    Dim destinationport As UInteger
+    Dim sourceport As UInteger
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        sizediff.Height = Me.Height - DGV.Height
+        sizediff.Width = Me.Width - DGV.Width
+        formloaded = True
+
+        mycomputerconnections = Net.NetworkInformation.NetworkInterface.GetAllNetworkInterfaces
+
+        For i = 0 To mycomputerconnections.Length - 1
+            ComboBox1.Items.Add(mycomputerconnections(i).Name)
+        Next
+    End Sub
+    Private Sub OnReceive(ByVal asyncresult As IAsyncResult)
+
+        If started = True Then
+            'Get Length of packet (including header)
+            Dim readlength As UInteger = BitConverter.ToUInt16(Byteswap(bytedata, 2), 0)
+            sourceport = BitConverter.ToUInt16(Byteswap(bytedata, 22), 0)
+            destinationport = BitConverter.ToUInt16(Byteswap(bytedata, 24), 0)
+
+            'Get Protocol Type
+            If bytedata(9) = 6 Then
+                Typez = "TCP"
+            ElseIf bytedata(9) = 17 Then
+                Typez = "UDP"
+            Else
+                Typez = "???"
+            End If
+
+            'Get IP from and to
+            ipfrom = New IPAddress(BitConverter.ToUInt32(bytedata, 12))
+            ipto = New IPAddress(BitConverter.ToUInt32(bytedata, 16))
+
+            'If this is a packet to/from me and not from myself then...
+            If (ipfrom.Equals(myip) = True Or ipto.Equals(myip) = True) And ipto.Equals(ipfrom) = False Then
+                If FilterIP = False Or (FilterIP = True And (FilterIPAddress.Equals(ipfrom) Or FilterIPAddress.Equals(ipto))) Then
+
+                    'Fix data
+                    stringz = ""
+                    For i = 26 To readlength - 1
+                        If Char.IsLetterOrDigit(Chr(bytedata(i))) = True Then
+                            stringz = stringz & Chr(bytedata(i))
+                        Else
+                            stringz = stringz & "."
+                        End If
+                    Next
+
+                    'Put data to DataGridView, since it's on a different thread now, invoke it
+                    DGV.Invoke(New MethodInvoker(AddressOf DGVUpdate))
+
+                End If
+            End If
+
+        End If
+
+        'Restart the Receiving
+        socketz.BeginReceive(bytedata, 0, bytedata.Length, SocketFlags.None, New AsyncCallback(AddressOf OnReceive), Nothing)
+    End Sub
+
+    Private Sub DGVUpdate()
+
+        'Remove rows if there are too many
+        If DGV.Rows.Count > 50 Then
+            DGV.Rows.RemoveAt(0)
+        End If
+        Dim row As String() = New String() {ipfrom.ToString & ":" & sourceport, ipto.ToString & ":" & destinationport, Typez, stringz}
+
+
+
+        DGV.Rows.Add(row)
+        'DGV.Rows(DGV.Rows.Count - 1).Cells(0).Value = ipfrom.ToString & ":" & sourceport 'From Column, size at 125
+        'DGV.Rows(DGV.Rows.Count - 1).Cells(1).Value = ipto.ToString & ":" & destinationport 'To Column, size at 125
+        'DGV.Rows(DGV.Rows.Count - 1).Cells(2).Value = Typez 'Type Column, size at 50
+        'DGV.Rows(DGV.Rows.Count - 1).Cells(3).Value = stringz 'Data column, size mode set to fill
+
+    End Sub
+
+    Private Function Byteswap(ByVal bytez() As Byte, ByVal index As UInteger)
+        Dim result(1) As Byte
+        result(0) = bytez(index + 1)
+        result(1) = bytez(index)
+        Return result
+    End Function
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If started = True Then
+            Button1.Text = "Start"
+            started = False
+        Else
+            Button1.Text = "Stop"
+            started = True
+        End If
+    End Sub
+
+    Private Sub Form1_Resize(sender As Object, e As EventArgs) Handles MyBase.Resize
+        If formloaded = True Then
+            DGV.Size = Me.Size - sizediff
+        End If
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+        Try
+            If TextBox1.Text <> "" And TextBox1.Text IsNot Nothing Then
+                FilterIPAddress = IPAddress.Parse(TextBox1.Text)
+                FilterIP = True
+                TextBox1.BackColor = Color.LimeGreen
+            Else
+                FilterIP = False
+                TextBox1.BackColor = Color.White
+            End If
+        Catch ex As Exception
+            FilterIP = False
+            TextBox1.BackColor = Color.White
+        End Try
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+        For i = 0 To mycomputerconnections(ComboBox1.SelectedIndex).GetIPProperties.UnicastAddresses.Count - 1
+
+            If mycomputerconnections(ComboBox1.SelectedIndex).GetIPProperties.UnicastAddresses(i).Address.AddressFamily = Net.Sockets.AddressFamily.InterNetwork Then
+
+                myip = mycomputerconnections(ComboBox1.SelectedIndex).GetIPProperties.UnicastAddresses(i).Address
+
+                BindSocket()
+
+            End If
+
+        Next
+    End Sub
+    Private Sub BindSocket()
+
+        Try
+            socketz.Bind(New IPEndPoint(myip, 0))
+            socketz.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.HeaderIncluded, True)
+            Dim bytrue() As Byte = {1, 0, 0, 0}
+            Dim byout() As Byte = {1, 0, 0, 0}
+            socketz.IOControl(IOControlCode.ReceiveAll, bytrue, byout)
+            socketz.Blocking = False
+            ReDim bytedata(socketz.ReceiveBufferSize)
+            socketz.BeginReceive(bytedata, 0, bytedata.Length, SocketFlags.None, New AsyncCallback(AddressOf OnReceive), Nothing)
+            ComboBox1.Enabled = False
+        Catch ex As Exception
+            ComboBox1.BackColor = Color.Red
+        End Try
+
+    End Sub
+
+End Class
